be54475fa03dded32ea5c95775a7e3e5abc6ce91
Tomás Arjovsky <t.arjovsky@gmail.com>
2023-12-27 15:39:15 -0300

    [Refactor] Genserver tracer (#2)
    * add tool versions
    
    * rename tool versions and bump elixir version
    
    * bump version
    
    * separate example from tracer without modifying general behavior.

--- lib/tracer.ex
+++ lib/tracer.ex
@@ -1,49 +1,33 @@
 defmodule Tracer do
   @moduledoc """
-  Documentation for `Tracer`.
+  A tracer module that receives :erlang.trace generated messages, parses them into
+  a local state and prints them to the screen.
   """
 
-  @doc """
-  Hello world.
+  use GenServer
 
-  ## Examples
+  ###########################
+  ### PUBLIC API
+  ###########################
 
-      iex> Tracer.hello()
-      :world
-
-  """
-  def hello do
-    :world
+  def trace(pid) do
+    GenServer.start_link(__MODULE__, pid)
   end
 
-  @dummy_list 0..1000 |> Enum.to_list()
-
-  def start do
-    tracee = spawn(fn -> dummy_load() end)
+  ###########################
+  ### GENSERVER CALLBACKS
+  ###########################
+  def init(pid) do
     match_spec = [{:_, [], [{:message, {{:cp, {:caller}}}}]}]
     :erlang.trace_pattern(:on_load, match_spec, [:local])
     :erlang.trace_pattern({:_, :_, :_}, match_spec, [:local])
-    :erlang.trace(tracee, true, [:call, :arity, :return_to, :timestamp, :running])
-
-    print_traces()
+    :erlang.trace(pid, true, [:call, :arity, :return_to, :timestamp, :running])
+    {:ok, %{}}
   end
 
-  defp print_traces do
-    receive do
-      t ->
-        :trace_ts = elem(t, 0)
-        IO.inspect(t)
-    end
-
-    print_traces()
-  end
-
-  defp dummy_load do
-    @dummy_list
-    |> Stream.with_index()
-    |> Stream.map(fn {x, y} -> x * y end)
-    |> Enum.reduce(0, fn x, acc -> x + acc end)
-
-    dummy_load()
+  def handle_info(t, state) do
+    :trace_ts = elem(t, 0)
+    IO.inspect(t)
+    {:noreply, state}
   end
 end
